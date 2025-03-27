6e4e56b6b99cb4fd191e623a23a423899364ef8a
Sniya Jose <sniyajose18@Gmail.com>
2025-03-27 09:38:40 +0530

    set Middleware

--- backend/src/constrollers/auth.controller.js
+++ backend/src/constrollers/auth.controller.js
@@ -84,3 +84,7 @@ export const logout = (req,res)=>{
     res.status(500).json({message:"Internal server error"})
   }
 }
+
+export const updateProfile = async(req,res)=>{
+
+}
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,10 +1,13 @@
 import express from "express";
-import {login,logout,signup} from "../constrollers/auth.controller.js"
+import {login,logout,signup,updateProfile} from "../constrollers/auth.controller.js";
+import {protectRoute} from "../midddleware/auth.middleware.js"
 
 const router = express.Router();
 
 router.post('/signup',signup)
 router.post('/login',login)
 router.post('/logout',logout)
+
+router.put("/update-profile",protectRoute,updateProfile)
 
 export default router;
