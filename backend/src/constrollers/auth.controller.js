6dc48f22c98b5b9936e0dbc8ecc93372b747654a
Sniya Jose <sniyajose18@Gmail.com>
2025-03-28 16:56:27 +0530

    add cloudinary and start frontend working

--- backend/src/constrollers/auth.controller.js
+++ backend/src/constrollers/auth.controller.js
@@ -86,5 +86,32 @@ export const logout = (req,res)=>{
 }
 
 export const updateProfile = async(req,res)=>{
+ try {
+    const {profilePic} = req.body
+    const userId = req.user._id;
 
+    if(!profilePic){
+        return res.status(400).json({message:"Profile pic is required"})
+    }
+
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+    const updateUser = await User.findByIdAndUpdate(
+        userId,
+        {profilePic:uploadResponse.secure_url},
+        {new:true}
+    );
+    res.status(200).json(updateUser);
+ } catch (error) {
+    console.log("error in update profile",error);
+    res.status(500).json({message:"Internal server error"})
+ }
+}
+
+export const checkAuth = (req,res)=>{
+    try{
+        res.status(200).json(req.user);  
+    }catch(error){
+        console.log("Error in CheckAuth",error.message);
+        res.status(500).json({message:"INternal server Error"})
+    }
 }
--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
+import messageRoute from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config()
@@ -15,6 +16,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use("/api/auth",authRoutes);
+app.use("/api/message",messageRoute);
 
 app.listen(PORT,()=>{
     console.log("server is running on port : " + PORT)
