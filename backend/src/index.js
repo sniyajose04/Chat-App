eeeafbf424936d210e531bad486dbd7be37ec64a
Sniya Jose <sniyajose18@Gmail.com>
2025-04-07 18:02:40 +0530

    backend and frontend connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,10 +2,12 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoute from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
+
 
 dotenv.config()
 const app = express();
@@ -14,6 +16,10 @@ const PORT = process.env.PORT
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
 
 app.use("/api/auth",authRoutes);
 app.use("/api/message",messageRoute);
