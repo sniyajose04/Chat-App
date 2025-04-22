e99a64154e7b6d48d7515d7d3bf81e8ae3ef7593
Sniya Jose <sniyajose18@Gmail.com>
2025-04-22 11:22:55 +0530

    fixing of homePage

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,50 +1,46 @@
-import React from "react";
-import { useAuthStore } from "../store/useAuthStore";
 import { Link } from "react-router-dom";
+import { useAuthStore } from "../store/useAuthStore";
 import { LogOut, MessageSquare, Settings, User } from "lucide-react";
-import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const { logout, authUser } = useAuthStore();
-  const navigate = useNavigate();
 
   return (
-    <header className="bg-base-100 border-b border-base-300 fixed w-full top-0 z-40 backdrop-blur-lg bg-base-100/80">
+    <header
+      className="bg-base-100 border-b border-base-300 fixed w-full top-0 z-40 
+    backdrop-blur-lg bg-base-100/80"
+    >
       <div className="container mx-auto px-4 h-16">
-        <div className="flex justify-between items-center gap-8 h-full">
-          {/* Left - Logo */}
-          <Link
-            to="/"
-            className="flex items-center gap-2.5 hover:opacity-80 transition-all"
-          >
-            <div className="size-9 rounded-lg bg-primary/10 flex items-center justify-center">
-              <MessageSquare className="size-5 text-primary" />
-            </div>
-            <h1 className="text-lg font-bold">Chatty</h1>
-          </Link>
+        <div className="flex items-center justify-between h-full">
+          <div className="flex items-center gap-8">
+            <Link to="/" className="flex items-center gap-2.5 hover:opacity-80 transition-all">
+              <div className="size-9 rounded-lg bg-primary/10 flex items-center justify-center">
+                <MessageSquare className="w-5 h-5 text-primary" />
+              </div>
+              <h1 className="text-lg font-bold">Chatty</h1>
+            </Link>
+          </div>
 
-          {/* Right - Buttons */}
           <div className="flex items-center gap-2">
-            <Link to="/settings" className="btn btn-sm gap-2 transition-colors">
-              <Settings className="size-4" />
+            <Link
+              to={"/settings"}
+              className={`
+              btn btn-sm gap-2 transition-colors
+              
+              `}
+            >
+              <Settings className="w-4 h-4" />
               <span className="hidden sm:inline">Settings</span>
             </Link>
 
             {authUser && (
               <>
-                <Link to="/profile" className="btn btn-sm gap-2">
+                <Link to={"/profile"} className={`btn btn-sm gap-2`}>
                   <User className="size-5" />
                   <span className="hidden sm:inline">Profile</span>
                 </Link>
 
-                <button
-                  type="button"
-                  className="btn btn-sm gap-2"
-                  onClick={() => {
-                    logout();
-                    navigate("/login");
-                  }}
-                >
+                <button className="flex gap-2 items-center" onClick={logout}>
                   <LogOut className="size-5" />
                   <span className="hidden sm:inline">Logout</span>
                 </button>
@@ -56,5 +52,4 @@ const Navbar = () => {
     </header>
   );
 };
-
 export default Navbar;
